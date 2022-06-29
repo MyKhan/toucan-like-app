5d5c3c39b584ff7d44b5f25c4e5002af9f845876
Meher Yar Khan <meheryar.khan@kone.com>
2022-06-29 19:30:37 +0300

    working add to list function and toast

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [meaningOfSearchTerm, setMeaningOfSearchTerm] = useState('');
+  const [list, setList] = useState([]); //contains list of dictionary objects
+  const input = useRef();
 
   useEffect(() => {
     document.getElementById("google_translate_element").textContent = meaningOfSearchTerm;
   }, [meaningOfSearchTerm]);
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
 
   const handleSetSearchTerm = (e) => {
     setSearchTerm(() => {
@@ -25,7 +33,20 @@ const App = () => {
   }
 
   const addToList = () => {
+    if (meaningOfSearchTerm.length === 0) return notify();
+    setList(() => {
+      return [...list, {searchTerm, meaningOfSearchTerm}];
+    });
+    setSearchTerm(() => {
+      return "";
+    });
+    setMeaningOfSearchTerm(() => {
+      return "";
+    })
+  }
 
+  const notify = () => {
+    toast("First search for the meaning!");
   }
 
   return (
@@ -33,12 +54,17 @@ const App = () => {
       <Form onSubmit={(e) => e.preventDefault()}>
         <Form.Group className="mb-3" controlId="searchterm">
           <Form.Label>Search Term</Form.Label>
-          <Form.Control type="text" placeholder="Payment" onChange={handleSetSearchTerm} />
+          <Form.Control value={searchTerm} type="text" placeholder="Payment" ref={input} onChange={handleSetSearchTerm} />
         </Form.Group>
       </Form>
       <div id="google_translate_element"></div>
       <Button variant="outline-primary" onClick={searchGoogleTranslate}>Find Meaning</Button>{' '}
       <Button variant="outline-primary" onClick={addToList}>Add to List</Button>{' '}
+      <ToastContainer
+        position="top-left"
+        theme="dark"
+        type = "error"
+      />
     </>
   )
 }
